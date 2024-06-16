cda8bfdb84ea4156161494a8df5f8624fa5f0f5c
axitasharma <cseakshita@gmail.com>
2024-06-16 20:07:09 +0530

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -1,1 +1,9 @@
 assignment 1
+def sum_two_numbers():
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+    
+    sum_result = num1 + num2
+    print(f"The sum of {num1} and {num2} is {sum_result}")
+
+sum_two_numbers()
